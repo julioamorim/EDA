f26aab6ac36c69fd45e631d1cd6c64582b7cb340
julioamorim <julio.l.amorim00@gmail.com>
2021-07-04 18:00:51 -0300

    Appliction mais class and tests addes

---
+++ src/main/java/io/julioamorim/EventDrivenArchitectureApplication.java
@@ -0,0 +1,13 @@
+package io.julioamorim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenArchitectureApplication.class, args);
+	}
+
+}
