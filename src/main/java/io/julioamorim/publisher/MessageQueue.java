c94e9e20e03fdaf018c5d9d72ea213d93e32933a
julioamorim <julio.l.amorim00@gmail.com>
2021-07-04 18:11:42 -0300

    MessageQue created

---
+++ src/main/java/io/julioamorim/publisher/MessageQueue.java
@@ -0,0 +1,31 @@
+package io.julioamorim.publisher;
+
+import org.apache.kafka.clients.producer.*;
+
+import java.util.Properties;
+import java.util.concurrent.Future;
+import java.util.logging.Logger;
+
+public class MessageQueue {
+    private static Properties properties;
+    private static final String EMAIL_TOPIC = "EMAIL_TOPIC";
+    private static final String KAFKA_IP = "127.0.0.1:9092";
+    private static final String SERIALIZER_CLASS = "org.apache.kafka.common.serialization.StringSerializer";
+    private static Logger logger = Logger.getLogger(MessageQueue.class.getName());
+
+    private static KafkaProducer<String, String> createKafkaProducer() {
+        properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_IP);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, SERIALIZER_CLASS);
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, SERIALIZER_CLASS);
+        return new KafkaProducer<String, String>(properties);
+    }
+
+    public static void sendEvent(String JSON) {
+        KafkaProducer<String, String> producer = createKafkaProducer();
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>(EMAIL_TOPIC, JSON);
+        Future<RecordMetadata> metadataFuture = producer.send(record);
+        producer.close();
+    }
+
+}
